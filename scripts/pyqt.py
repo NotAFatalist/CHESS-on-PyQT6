98acc9a0d13749b07beda20a465aae1e7950c656
Eldar <167672173+NotAFatalist@users.noreply.github.com>
2024-11-15 19:22:32 +0300

    Update pyqt.py

--- scripts/pyqt.py
+++ scripts/pyqt.py
@@ -5,28 +5,52 @@ import chess
 from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtGui import QPainter, QColor, QPixmap, QIcon
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QPushButton, QFileDialog
+from PyQt6.QtWidgets import QInputDialog
+
+
+imagemw = 'media/death-note-l-and-light-playing-chess-ft7rtfi086yvefyc.jpg'
+imageplay = 'media/sakura.webp'
+font_size = '17px'
+font = 'Comic Sans MS'
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        global imagemw, font_size
         self.setWindowTitle('Главное меню')
-        self.setGeometry(100, 100, 600, 600)
         layout = QVBoxLayout()
         button_playnew = QPushButton('Новая партия')
         button_playold = QPushButton('Старая партия')
         button_settings = QPushButton('Настройки')
-        button_playnew.setMaximumSize(100, 50)
-        button_playold.setMaximumSize(100, 50)
-        button_settings.setMaximumSize(100, 50)
+        button_playnew.setMaximumSize(300, 150)
+        button_playold.setMaximumSize(300, 150)
+        button_settings.setMaximumSize(300, 150)
+        button_playnew.setMinimumSize(100, 50)
+        button_playold.setMinimumSize(100, 50)
+        button_settings.setMinimumSize(100, 50)
+        button_settings.setStyleSheet(f"""
+                font-family: {font};
+                font-size: {font_size};
+        """)
+        button_playold.setStyleSheet(f"""
+                font-family: {font};
+                font-size: {font_size};
+        """)
+        button_playnew.setStyleSheet(f"""
+                font-family: {font};
+                font-size: {font_size};
+        """)
         layout.addWidget(button_playnew)
         layout.addWidget(button_playold)
         layout.addWidget(button_settings)
         layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         central_widget = QWidget()
-        image_path = 'media/death-note-l-and-light-playing-chess-ft7rtfi086yvefyc.jpg'
         central_widget.setStyleSheet(f'''
-            background-image: url({image_path});
+            background-image: url({imagemw});
             background-position: center;
             background-repeat: no-repeat;
             background-size: cover;
@@ -38,10 +62,18 @@ class MainWindow(QMainWindow):
         button_settings.clicked.connect(self.settings)
 
     def new_play(self):
-        self.mw = New_play()
-        self.mw.show()
-        self.close()
-
+        opponent, ok_pressed = QInputDialog.getItem(
+            self, "Выбор противника", "Оппонент",
+            ('Робот', 'Человек'), 1, False)
+        if ok_pressed:
+            if opponent == 'Человек':
+                self.mw = New_play()
+                self.mw.show()
+                self.close()
+            else:
+                self.mw = New_play(vsrobot=True)
+                self.mw.show()
+                self.close()
     def old_play(self):
         self.mw = Old_play()
         self.mw.show()
@@ -55,28 +87,149 @@ class MainWindow(QMainWindow):
 class Old_play(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        global font_size, font
         self.setWindowTitle('Выбор старой игры')
         self.setGeometry(100, 100, 600, 600)
         fname = QFileDialog.getOpenFileName(
             self, 'Выбрать картинку', '',
             'Картинка (*.jpg);;Картинка (*.png);;Все файлы (*)')
         # тут будет расшифровка cvv файла в доску, но позже
+        self.button = QPushButton('BACK', self)
+        self.button.clicked.connect(self.back)
+        self.button.move(0, 0)
+        self.button.resize(100, 50)
+        self.button.setStyleSheet(f"""
+                color: red;
+                background-color: black;
+                font-family: {font};
+                font-size: {font_size};
+            """)
+
+    def back(self):
+        self.mw = MainWindow()
+        self.mw.showMaximized()
+        self.close()
 
 class Settings(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Выбор старой игры')
+        self.initUI()
+
+    def initUI(self):
+        global font_size, font
+        self.setWindowTitle('Настройки')
         self.setGeometry(100, 100, 600, 600)
+        self.setFixedSize(500, 500)
         # тут будут настройки, описанные в README
+        layout = QVBoxLayout()
+        central_widget = QWidget()
+        image_path = 'media/settings.webp'
+        central_widget.setStyleSheet(f'''
+            background-image: url({image_path});
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        ''')
+        central_widget.lower()
+        button_chooseimagemw = QPushButton('Установить изображение на задний фон главного экрана')
+        button_chooseimageplay = QPushButton('Установить изображение на задний фон партии')
+        button_font = QPushButton('Изменить шрифт')
+        button_font_size = QPushButton('Изменить размер шрифта')
+        button_chooseimagemw.setMinimumSize(100, 50)
+        button_chooseimageplay.setMinimumSize(100, 50)
+        button_font_size.setMinimumSize(100, 50)
+        button_font.setMinimumSize(100, 50)
+        button_font_size.setStyleSheet(f"""
+            color: white;
+            font-family: {font};
+            font-size: {font_size};
+        """)
+        button_font.setStyleSheet(f"""
+            color: white;
+            font-family: {font};
+            font-size: {font_size};
+        """)
+        button_chooseimageplay.setStyleSheet(f"""
+            color: white;
+            font-family: {font};
+            font-size: {font_size};
+        """)
+        button_chooseimagemw.setStyleSheet(f"""
+            color: white;
+            font-family: {font};
+            font-size: {font_size};
+        """)
+        layout.addWidget(button_chooseimagemw)
+        layout.addWidget(button_chooseimageplay)
+        layout.addWidget(button_font)
+        layout.addWidget(button_font_size)
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+        button_chooseimagemw.clicked.connect(self.chooseimagemw)
+        button_chooseimageplay.clicked.connect(self.chooseimageplay)
+        button_font.clicked.connect(self.font_)
+        button_font_size.clicked.connect(self.font_size)
+        self.button = QPushButton('BACK', self)
+        self.button.clicked.connect(self.back)
+        self.button.move(0, 0)
+        self.button.resize(100, 50)
+        self.button.setStyleSheet(f"""
+                                font-family: {font};
+                                font-size: {font_size};
+                                color: red;
+                                background-color: black;
+                                """)
+
+    def back(self):
+        self.mw = MainWindow()
+        self.mw.showMaximized()
+        self.close()
+
+    def chooseimagemw(self):
+        global imagemw
+        imagemw = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.jpg);;Картинка (*.png)')[0]
+
+    def chooseimageplay(self):
+        global imageplay
+        imageplay = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.jpg);;Картинка (*.png)')[0]
+
+    def font_(self):
+        global font
+        name, ok_pressed = QInputDialog.getText(self, "Изменение шрифта",
+                                                "Введите шрифт")
+        if ok_pressed:
+            font = name
+            self.back()
+
+    def font_size(self):
+        global font_size
+        size, ok_pressed = QInputDialog.getInt(self, 'Изменение размера шрифта', 'Введите размер шрифта в px')
+        if ok_pressed:
+            if size <= 17:
+                font_size = str(size) + 'px'
+                self.back()
+            else:
+                font_size = '17px'
+                self.back()
+
 
 class New_play(QMainWindow):
-    def __init__(self):
+    def __init__(self, vsrobot=True):
         super().__init__()
+        self.initUI()
+        self.vsrobot = vsrobot
+
+    def initUI(self):
+        global imageplay, font, font_size
         self.setWindowTitle('Шахматная доска')
         self.setGeometry(100, 100, 600, 600)
         self.background_widget = QWidget(self)
-        self.background_widget.setStyleSheet('''
-            background-image: url(media/sakura.webp);
+        self.background_widget.setStyleSheet(f'''
+            background-image: url({imageplay});
             background-position: center;
             background-repeat: no-repeat;
             background-size: cover;
@@ -90,11 +243,31 @@ class New_play(QMainWindow):
         self.setCentralWidget(central_widget)
         self.background_widget.resize(self.size())
         self.background_widget.lower()
+        self.button = QPushButton('BACK', self)
+        self.button.clicked.connect(self.back)
+        self.button.move(0, 0)
+        self.button.resize(100, 50)
+        self.button.setStyleSheet(f"""
+            color: red;
+            background-color: black;
+            font-family: {font};
+            font-size: {font_size}
+        """)
         self.resizeEvent = lambda event: self.background_widget.resize(event.size())
 
+    def back(self):
+        self.mw = MainWindow()
+        self.mw.showMaximized()
+        self.close()
+
+
 class ChessBoard(QWidget):
-    def __init__(self):
+    def __init__(self, vs_robot=False):
         super().__init__()
+        self.vs_robot = vs_robot
+        self.initUI()
+
+    def initUI(self):
         self.setWindowTitle('Шахматная доска')
         self.setFixedSize(640, 640)
         self.board_size = 8
@@ -102,18 +275,18 @@ class ChessBoard(QWidget):
         self.highlighted_squares = []
         self.board = chess.Board()
         self.pieces = {
-            'P': QPixmap('media/figures/wp.png'),
-            'R': QPixmap('media/figures/wl.png'),
-            'N': QPixmap('media/figures/wh.png'),
-            'B': QPixmap('media/figures/wb.png'),
-            'Q': QPixmap('media/figures/wq.png'),
-            'K': QPixmap('media/figures/wk.png'),
-            'p': QPixmap('media/figures/bp.png'),
-            'r': QPixmap('media/figures/bl.png'),
-            'n': QPixmap('media/figures/bh.png'),
-            'b': QPixmap('media/figures/bb.png'),
-            'q': QPixmap('media/figures/bq.png'),
-            'k': QPixmap('media/figures/bk.png')
+            'P': QPixmap('figures/wp.png'),
+            'R': QPixmap('figures/wl.png'),
+            'N': QPixmap('figures/wh.png'),
+            'B': QPixmap('figures/wb.png'),
+            'Q': QPixmap('figures/wq.png'),
+            'K': QPixmap('figures/wk.png'),
+            'p': QPixmap('figures/bp.png'),
+            'r': QPixmap('figures/bl.png'),
+            'n': QPixmap('figures/bh.png'),
+            'b': QPixmap('figures/bb.png'),
+            'q': QPixmap('figures/bq.png'),
+            'k': QPixmap('figures/bk.png')
         }
         self.buttons = {}
         layout = QGridLayout()
@@ -148,9 +321,9 @@ class ChessBoard(QWidget):
                         pixmap = pixmap.scaled(80, 80, Qt.AspectRatioMode.KeepAspectRatio)
                         button.setIcon(QIcon(pixmap))
                 if self.selected_square and (row, col) == self.selected_square:
-                    button.setStyleSheet('background-color: rgba(0, 250, 154, 100);')
+                    button.setStyleSheet('background-color: rgba(0, 250, 154, 255);')
                 elif (row, col) in self.highlighted_squares:
-                    button.setStyleSheet('background-color: rgba(0, 255, 0, 100);')
+                    button.setStyleSheet('background-color: rgba(0, 255, 0, 255);')
                 else:
                     button.setStyleSheet(self.get_button_color(row, col))
 
@@ -161,13 +334,33 @@ class ChessBoard(QWidget):
             row, col = pos
             square = chess.square(col, 7 - row)
             piece = self.board.piece_at(square)
-            if piece:
+            if not self.selected_square and piece is not None and piece.color == self.board.turn:
+                # Если выбрана фигура, то подсвечиваем возможные ходы
                 self.selected_square = (row, col)
                 self.highlighted_squares = self.get_legal_moves(square)
-            else:
+            elif self.selected_square:
+                # Если выбрана пустая клетка, проверяем возможность хода
+                if (row, col) in self.highlighted_squares:
+                    from_square = chess.square(self.selected_square[1], 7 - self.selected_square[0])
+                    to_square = square
+                    move = chess.Move(from_square, to_square)
+                    if move in self.board.legal_moves:
+                        self.make_move(move)
                 self.selected_square = None
                 self.highlighted_squares = []
             self.update_board()
+
+    def make_move(self, move):
+        self.board.push(move)
+        self.update_board()
+        if self.vs_robot and self.board.turn == chess.BLACK:
+            self.move_black()  # Вызываем метод для хода черных
+
+    def move_black(self):
+        print("Черные ходят!")
+        # Здесь вы можете добавить свою реализацию искусственного интеллекта
+        # Пока что просто пропускаем ход
+        pass
 
     def get_button_position(self, button):
         for (row, col), btn in self.buttons.items():
@@ -184,7 +377,6 @@ class ChessBoard(QWidget):
                 to_row, to_col = divmod(to_square, 8)
                 legal_moves.append((7 - to_row, to_col))
         return legal_moves
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
